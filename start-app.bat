c6c615ff1127864d41286b4723157bb58a85e690
nixiz0 <julian.57@outlook.fr>
2024-01-23 13:08:32 +0100

    Improve the automatic .bat script

--- start-app.bat
+++ start-app.bat
@@ -1,4 +1,11 @@
 @echo off
+IF NOT EXIST .env (
+    python -m venv .env
+    cd .env\Scripts
+    call activate.bat
+    cd ../..
+    pip install -r requirements.txt
+)
 cd .env\Scripts
 call activate.bat
 cd ../..
